ad4ad4f60f0b04fb702d7cfa9d8741d66d8d6b79
Ma-cchiato <122413511+Ma-cchiato@users.noreply.github.com>
2023-12-31 19:35:29 +0900

    Update startup_nfs.sh

--- Script/startup_nfs.sh
+++ Script/startup_nfs.sh
@@ -4,6 +4,18 @@ directory=/home/deck/NFS
 source "$directory/NFS_settings.sh"
 NFS_log="$directory/NFS_log.log"
 
+chk_dir() {
+
+    if [ -d "$Mount_path" ]; then
+        echo -e "\n"
+        echo -e "Verify directory $Mount_path exists.\n"
+    else 
+        echo -e "\n"
+        echo -e "$(date "+%y-%m-%d %H:%M:%S") [Startup Script] Directory $Mount_path could not be found. Create a new directory.\n"
+        sudo mkdir -p $Mount_path
+    fi
+}
+
 umount_nfs () {
 
 sudo umount $Mount_path
@@ -11,11 +23,22 @@ echo "$(date "+%y-%m-%d %H:%M:%S") [Startup Script] Directory $Mount_path is unm
 
 }
 
-Current_SSID=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d: -f2)
+mount_nfs() {
+    Current_SSID=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d: -f2)
+    chk_dir
+    if [ "$Current_SSID" == "$Set_SSID" ]; then
+        sudo mount -t nfs $NFS_server_IP:$NFS_share_path $Mount_path
+        
+        if mount | grep -q "$Mount_path"; then
+            echo "$(date "+%y-%m-%d %H:%M:%S") [Startup Script] NFS Mount Success. ($NFS_server_IP) Current SSID: $Current_SSID" >> $NFS_log
+        else
+            umount_nfs
+            echo "$(date "+%y-%m-%d %H:%M:%S") [Startup Script] NFS Mount Failed. ($NFS_server_IP) Current SSID: $Current_SSID" >> $NFS_log
+        fi
+    else
+        umount_nfs
+        echo "$(date "+%y-%m-%d %H:%M:%S") [Startup Script] NFS Mount Failed. ($NFS_server_IP) Current SSID: $Current_SSID" >> $NFS_log
+    fi
+}
 
-if [ "$Current_SSID" != "$Set_SSID" ]; then
-    echo "$(date "+%y-%m-%d %H:%M:%S") [Startup Script] NFS Mount Failed!! ($NFS_server_IP) Current SSID: $Current_SSID" >> $NFS_log
-    umount_nfs
-else
-    echo "$(date "+%y-%m-%d %H:%M:%S") [Startup Script] NFS Mount Success!! ($NFS_server_IP) Current SSID: $Current_SSID" >> $NFS_log
-fi
+mount_nfs
